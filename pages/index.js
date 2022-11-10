8058e20800bb632e8a30ae00b3fdb96353873607
Dimitri M <ibmiti@gmail.com>
2022-11-10 10:32:16 -0500

    adding styles from other proj.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-export const index = () => {
+export const Home = () => {
   return (
     <>
         HeroBanner
+        <div>
+            <h2>Best Selling Products</h2>
+            <p>Speakers of many variations</p>
+        </div>
+
+        <div>
+           {
+            ['Product 1', 'Product 2'].map(
+            (product) => product)
+            }
+        </div>
     </>
+
   )
 }
 export default Home
