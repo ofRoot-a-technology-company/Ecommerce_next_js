4b11800237917aa4c43075c7234dcddb44ac2cad
Dimitri M <ibmiti@gmail.com>
2022-11-10 07:41:32 -0500

    adding babel to the code base

---
+++ pages/api/hello.js
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+
+// changed the extensions of this file from .ts to .js, may change back later if
+// going back to ts
